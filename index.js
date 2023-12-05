7bae4de3f7c8788000497e6def7dc06af43b6669
Yarolsav <kiritok712@gmail.com>
2023-12-05 15:56:56 +0100

    isDone

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { Telegraf, Markup, Extra } = require('telegraf');
+const path = require('path');
+const { Telegraf, Markup } = require('telegraf');
 const fetch = require('node-fetch');
 require('dotenv').config();
 
@@ -6,60 +7,63 @@ const { BOT_TOKEN } = process.env;
 const bot = new Telegraf(BOT_TOKEN);
 
 const api_teams = "https://www.balldontlie.io/api/v1/teams";
-const api_players = 'https://www.balldontlie.io/api/v1/players'; // це тобі на майбутнє, цей ендпоінт поверне всіх гравців
+const api_players = "https://www.balldontlie.io/api/v1/players";
 
 let dataFromServer = [];
 
 function getDataFromServer(forceFetch = false, api) {
   if (!forceFetch) {
-      return;
+    return;
   }
 
-  return fetch(api, 
-  {
-      method: 'GET', 
-      headers: {'Content-Type': 'application/json'}
+  return fetch(api, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
   })
-  .then(response => response.json())
-  .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       dataFromServer = data.data;
       console.log(dataFromServer);
-  })   
+    });
 }
 
-
-bot.start(ctx => {
+bot.start((ctx) => {
   ctx.replyWithHTML("Welcome to my bot", Markup.inlineKeyboard([
     Markup.button.callback('Всі команди', 'allTeams'),
-    Markup.button.callback('Button 2', 'button2'), //тут зроби "всі гравці"
-    Markup.button.callback('Button 3', 'button3'), // тут зроби "пошук гравця"
-  ]))
+    Markup.button.callback('Пошук гравця', 'button3'),
+  ]));
 });
 
-
-bot.hears(/hi/i, (ctx) => {ctx.reply('Hi to you too')})
-
 let currentIndex = 0;
-let sentMessageId = null;
+
 
 async function sendTeamInfo(ctx) {
   const team = dataFromServer[currentIndex];
 
   const message = `
     <b>${team.full_name}</b>
+    Abbreviation: ${team.abbreviation}
     City: ${team.city}
-    `;
-//тут всі поля виведи
+    Name: ${team.full_name}
+    Division: ${team.division}
+  `;
 
-  await ctx.telegram.editMessageCaption(
-      ctx.chat.id,
-      sentMessageId,
-      null,
-      message,
-      { parse_mode: 'HTML', ...Markup.inlineKeyboard([
+
+  const imagePath = path.join(__dirname, 'images', `${team.id}.png`);
+  const imageBuffer = require('fs').readFileSync(imagePath);
+
+  await ctx.telegram.sendDocument(
+    ctx.chat.id,
+    { source: imageBuffer, filename: 'image.png' },
+    {
+      caption: message,
+      parse_mode: 'HTML',
+      ...Markup.inlineKeyboard([
         [{ text: 'Попередня команда', callback_data: 'previous' }],
+        [{ text: 'Початкове меню', callback_data: 'mainMenu' }],
         [{ text: 'Наступна команда', callback_data: 'next' }],
-      ])}
+      ]),
+    }
   );
 }
 
@@ -73,54 +77,68 @@ bot.action('next', (ctx) => {
   sendTeamInfo(ctx);
 });
 
-//це зараз працює і красиво показує команди
+
+
 bot.action('allTeams', async (ctx) => {
-  await getDataFromServer(dataFromServer.length == 0, api_teams);
-  const team = dataFromServer[currentIndex];
-
-  const message = `
-    <b>${team.full_name}</b>
-    City: ${team.city}
-
-    `;
-//тут всі поля виведи
-
-    const sentMessage = await ctx.replyWithPhoto({ url: 'https://robohash.org/1' }, {
-        caption: message,
-        parse_mode: 'HTML',
-        reply_markup: {
-            inline_keyboard: [
-                [{ text: 'Попередня команда', callback_data: 'previous' }],
-                [{ text: 'Наступна команда', callback_data: 'next' }],
-            ],
-        },
-    });
-
-    // Store the sent message ID
-    sentMessageId = sentMessage.message_id;
-
+  await getDataFromServer(dataFromServer.length === 0, api_teams);
+  sendTeamInfo(ctx);
 });
 
-bot.action('previous', (ctx) => {
-  currentIndex = (currentIndex - 1 + data.length) % data.length;
-  sendPlayerInfo(ctx);
-});
-
-bot.action('next', (ctx) => {
-  currentIndex = (currentIndex + 1) % data.length;
-  sendPlayerInfo(ctx);
-});
-
-
-// це нижче тобі заготовки
-bot.action('button2', (ctx) => {
-  ctx.reply('You clicked Button 2!');
-});
 
 bot.action('button3', (ctx) => {
-  ctx.reply('You clicked Button 3!');
+  ctx.reply('Введіть ім\'я та прізвище гравця (наприклад, LeBron James або Alex Abrines):');
+
+
+  const textHandler = async (ctx) => {
+    const fullName = ctx.message.text.trim();
+
+
+    await searchPlayer(fullName, ctx);
+  };
+
+
+  bot.on('text', textHandler);
+});
+
+
+async function searchPlayer(fullName, ctx) {
+  const response = await fetch(`${api_players}?search=${encodeURIComponent(fullName)}`);
+  const data = await response.json();
+
+  const foundPlayers = data.data;
+
+  if (foundPlayers.length === 0) {
+    ctx.reply(`Гравця ${fullName} не знайдено.`);
+  } else {
+
+    for (const player of foundPlayers) {
+      await sendPlayerInfo(ctx, player);
+    }
+  }
+}
+
+
+async function sendPlayerInfo(ctx, player) {
+  const message = `
+    <b>${player.first_name} ${player.last_name}</b>
+    Позиція: ${player.position}
+    Команда: ${player.team.full_name}
+  `;
+
+  await ctx.replyWithHTML(message, Markup.inlineKeyboard([
+    [{ text: 'Початкове меню', callback_data: 'mainMenu' }],
+  ]));
+}
+
+
+bot.action('mainMenu', (ctx) => {
+  ctx.replyWithHTML("З поверненням!", Markup.inlineKeyboard([
+    Markup.button.callback('Всі команди', 'allTeams'),
+    Markup.button.callback('Пошук гравця', 'button3'),
+  ]));
 });
 
 
 
 bot.launch();
+

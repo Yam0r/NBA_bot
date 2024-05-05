0bd5d59fe863a980bc90418611970e302f62dab8
Yarolsav <kiritok712@gmail.com>
2024-05-05 14:39:57 +0200

    actual_update

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const { Telegraf, Markup } = require('telegraf');
-const path = require('path');
-const { Telegraf, Markup } = require('telegraf');
 const fetch = require('node-fetch');
 require('dotenv').config();
 
